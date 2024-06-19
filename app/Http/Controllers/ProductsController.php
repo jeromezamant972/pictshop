3eef211e8ac3658bbc7e390bc15aae5621c33dc8
@jeromezamant972 <jeromezamant@hotmail.com>
2024-06-19 11:01:42 -0400

    modified

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Products;
 use Illuminate\Http\Request;
 
 class ProductsController extends Controller
 {
-    //
+    // faire une incentiation
+    public function index (){
+        $produits = Products::all();
+        return view('product.layouts.app',compact('produits'));
+    }
 }
