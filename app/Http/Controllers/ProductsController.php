f774867b95e891123d80f98347a0d08f956ad575
@jeromezamant972 <jeromezamant@hotmail.com>
2024-06-19 11:09:19 -0400

    modified

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -12,4 +12,13 @@ class ProductsController extends Controller
         $produits = Products::all();
         return view('product.layouts.app',compact('produits'));
     }
+
+    public function store(Request $request){
+        $produit = Products::create([
+            'name'=>$request->input('name'),
+            'description'=>$request->input('description'),
+            'price'=>$request->input('price')
+       ]);
+       return redirect('/');
+    }
 }
